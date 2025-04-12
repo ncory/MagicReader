777d1456e1035875b10f7dc837ff5205053a5296
Nate Cory <nate@natecory.com>
2025-04-12 13:25:34 -0400

    Testing with USB serial reader

--- rfid-testing/serial-test.py
+++ rfid-testing/serial-test.py
@@ -1,7 +1,17 @@
 import serial
+from serial.tools import list_ports
 
+from bandManager import BandManager
 
 try:
+    # Create band manager
+    band_manager = BandManager()
+
+    print("Available serial ports:")
+    port = list(list_ports.comports())
+    for p in port:
+        print(f"\t{p.device}")
+
     # open serial port
     print("Opening serial port...")
     ser = serial.Serial('/dev/ttyUSB0')
@@ -14,10 +24,16 @@ try:
         print("Waiting for RFID read...")
         # Read to next line
         line = ser.readline()
-        # Convert to string
-        line = line.decode('utf-8').strip()
-        # Print read line
-        print(f"Read RFID: {line}")
+        print(f"Read data: {line}")
+        # Convert to ASCII
+        ascii = line.decode('ascii').strip()
+        print(f"\tASCII: {ascii}")
+        # Convert to UTF-8
+        utf8 = line.decode('utf-8').strip()
+        print(f"\tUTF-8: {utf8}")
+        # Does it match as a Disney band?
+        if (band_manager.isIdDisneyBand(utf8)):
+            print("\tIs a Disney band")
 
 except KeyboardInterrupt:
     print("Caught keyboard interrupt...exiting")
